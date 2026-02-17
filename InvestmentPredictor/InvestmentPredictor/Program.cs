fd4a23247afaaec0176ed869944b65ae04cb41f9
mikolajj04 <Mikolajjussak@gmail.com>
2026-02-17 23:05:33 +0100

    feat: add initial, basic investment predictor logic with input validation

---
+++ InvestmentPredictor/InvestmentPredictor/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace InvestmentPredictor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-Investment Predictor v0.1-");
+            Console.WriteLine("Enter initial investment amount: ");
+            
+            
+            if(!decimal.TryParse(Console.ReadLine(), out decimal initialAmount))
+            {
+                Console.Error.WriteLine("Enter proper value!");
+                return;
+            }
+            Console.WriteLine("How long do you want to invest? (years): ");
+           if(!int.TryParse(Console.ReadLine(), out int period))
+            {
+                Console.Error.WriteLine("Enter proper period of time!");
+                return;
+            }
+            Console.WriteLine($"How much do you want to subsidize monthly?: ");
+           if(!decimal.TryParse(Console.ReadLine(), out decimal monthlySubsidy))
+            {
+                Console.Error.WriteLine("Enter proper value!");
+                return;
+            }
+
+            Console.WriteLine("Enter expected annual return rate (e.g. 10 for 10%): ");
+            if(!decimal.TryParse(Console.ReadLine(), out decimal annualReturn))
+            {
+                Console.Error.WriteLine("Enter proper annual return rate!");
+                return;
+            }
+
+          decimal  calculatedValue = 0 + initialAmount;
+            for(int year=1 ; year <= period; year++)
+            {
+                for(int month=1; month<=12; month++)
+                {
+                    calculatedValue += monthlySubsidy;
+                    calculatedValue += calculatedValue * annualReturn / 100 / 12;
+                }
+                
+                
+            }
+            
+            Console.WriteLine($"Total value after {period} years:  {calculatedValue:C} ");
+        }
+
+    }
+}
