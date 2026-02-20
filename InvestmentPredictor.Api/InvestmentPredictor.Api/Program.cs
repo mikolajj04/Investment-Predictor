3a1aa3ff5c828480b19980b8f2ef478c9674f502
mikolajj04 <Mikolajjussak@gmail.com>
2026-02-20 20:53:25 +0100

    feat: Dependency Injection practice

--- InvestmentPredictor.Api/InvestmentPredictor.Api/Program.cs
+++ InvestmentPredictor.Api/InvestmentPredictor.Api/Program.cs
@@ -1,3 +1,5 @@
+using InvestmentPredictor;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<InvestmentCalculator>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
