6910756710608609280d9564875f1d93111f4313
mikolajj04 <Mikolajjussak@gmail.com>
2026-02-22 22:12:10 +0100

    feat: add selection screen for indexes

--- InvestmentPredictor/InvestmentPredictor/Program.cs
+++ InvestmentPredictor/InvestmentPredictor/Program.cs
@@ -5,11 +5,44 @@ namespace InvestmentPredictor
     {
         static void Main(string[] args)
         {
+            var calculator = new InvestmentCalculator();
             Console.WriteLine("-Investment Predictor v0.1-");
+            Console.WriteLine("Select one of following indexes or choose custom option for custom ROI: ");
+            Console.WriteLine("1. S&P 500 - 10.5%");
+            Console.WriteLine("2. Nasdaq100 - 14,4%");
+            Console.WriteLine("3. WIG20 - 6,2%");
+            Console.WriteLine("4. Gold - 7,8%");
+            Console.WriteLine("5. Custom");
+
+            decimal annualReturn;
+            ConsoleKeyInfo option = Console.ReadKey(true);
+            char charOption = option.KeyChar;
+            MarketIndex index = MarketIndex.Custom;
+            if (char.IsDigit(charOption))
+            {
+                if ((MarketIndex)char.GetNumericValue(charOption)== MarketIndex.Custom)
+                {
+                    Console.WriteLine("Enter expected annual return rate (e.g. 10 for 10%): ");
+                    if (!decimal.TryParse(Console.ReadLine(), out  annualReturn))
+                    {
+                        Console.Error.WriteLine("Enter proper annual return rate!");
+                        return;
+                    }
+                }
+                else { 
+                index = (MarketIndex)(int)char.GetNumericValue(charOption);
+                annualReturn = InvestmentCalculator.GetAnnualReturn(index);
+                }
+            }else{
+
+                Console.Error.WriteLine("Invalid option! ");
+                return;
+            }
+          
+
+
             Console.WriteLine("Enter initial investment amount: ");
-            
-            
-            if(!decimal.TryParse(Console.ReadLine(), out decimal initialAmount))
+            if (!decimal.TryParse(Console.ReadLine(), out decimal initialAmount))
             {
                 Console.Error.WriteLine("Enter proper value!");
                 return;
@@ -27,13 +60,16 @@ namespace InvestmentPredictor
                 return;
             }
 
-            Console.WriteLine("Enter expected annual return rate (e.g. 10 for 10%): ");
-            if(!decimal.TryParse(Console.ReadLine(), out decimal annualReturn))
-            {
-                Console.Error.WriteLine("Enter proper annual return rate!");
-                return;
-            }
-            var calculator = new InvestmentCalculator();
+
+          
+
+
+            //Console.WriteLine("Enter expected annual return rate (e.g. 10 for 10%): ");
+            //if(!decimal.TryParse(Console.ReadLine(), out decimal annualReturn))
+            //{
+            //    Console.Error.WriteLine("Enter proper annual return rate!");
+            //    return;
+            //}
             decimal calculatedValue = calculator.CalculatedValue(monthlySubsidy, initialAmount, annualReturn, period);
             
             
