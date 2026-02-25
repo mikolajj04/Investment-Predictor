3effc8583fec7d6aa309c4274736eab5a02c109e
mikolajj04 <Mikolajjussak@gmail.com>
2026-02-25 22:42:10 +0100

    feat: add prototype .wpf project

---
+++ InvestmentPredictor.wpf/InvestmentPredictor.wpf/InvestmentCalculator.cs
@@ -0,0 +1,38 @@
+﻿namespace InvestmentPredictor
+{
+    public class InvestmentCalculator : IInvestmentCalculator
+    {
+        public decimal GetIndexAnnualReturn(MarketIndex index) => index switch
+        {
+            MarketIndex.SP500 => 10.4m,
+            MarketIndex.Nasdaq100 => 16.2m,
+            MarketIndex.WIG20 => 5.2m,
+            MarketIndex.Gold => 8.5m,
+            MarketIndex.MSCIWorld => 8.9m,
+            MarketIndex.Russell2000=> 8.2m,
+            MarketIndex.MSCIEmergingMarkets => 5.9m,
+            MarketIndex.DowJones => 7.4m,
+            _ => 0m
+
+
+        };
+       public decimal CalculatedValue(decimal monthlySubsidy, decimal initialAmount, decimal annualReturn, int period)
+        {
+            decimal calculatedValue = initialAmount;
+            for (int year = 1; year <= period; year++)
+            {
+                for (int month = 1; month <= 12; month++)
+                {
+                    calculatedValue += monthlySubsidy;
+                    calculatedValue += calculatedValue * annualReturn / 100 / 12;
+                }
+
+
+            }
+            return calculatedValue;
+        }
+
+       
+
+    }
+}
