3652c4c2a8d24c2a1205e2f8c21bcebd1dacd2c2
mikolajj04 <mikolajjussak@gmail.com>
2026-03-03 12:56:12 +0100

    refactor: add record that contains all of user-data parameters

--- InvestmentPredictor/InvestmentPredictor.Core/IInvestmentCalculator.cs
+++ InvestmentPredictor/InvestmentPredictor.Core/IInvestmentCalculator.cs
@@ -6,14 +6,16 @@ using System.Threading.Tasks;
 
 namespace InvestmentPredictor
 {
+
+    public record CalculatorParams(decimal monthlySubsidy, decimal initialAmount, decimal annualReturn, int period);
     public interface IInvestmentCalculator
     {
         decimal GetIndexAnnualReturn(MarketIndex index);
-        decimal CalculatedValue(decimal monthlySubsidy, decimal initialAmount, decimal annualReturn, int period);
+        decimal CalculatedValue(CalculatorParams p);
 
-        decimal getPureReturnValue(decimal monthlySubsidy, decimal initialAmount, decimal annualReturn, int period);
+        decimal getPureReturnValue(CalculatorParams p);
 
-        decimal CalculateTaxValue(decimal monthlySubsidy, decimal initialAmount, decimal annualReturn, int period);
-        decimal totalValueAfterTax(decimal monthlySubsidy, decimal initialAmount, decimal annualReturn, int period);
+        decimal CalculateTaxValue(CalculatorParams p);
+        decimal totalValueAfterTax(Func<decimal> calculateValue, Func<decimal> pureProfit, Func<decimal> pureProfitAfterTax);
     }
 }
