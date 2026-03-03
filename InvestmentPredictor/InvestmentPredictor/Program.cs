62f615d10ad3558e204546a37ce9a30c28c30e90
mikolajj04 <mikolajjussak@gmail.com>
2026-03-03 14:41:49 +0100

    refactor: replace all methods parameters with CalculatorParams record

--- InvestmentPredictor/InvestmentPredictor/Program.cs
+++ InvestmentPredictor/InvestmentPredictor/Program.cs
@@ -7,25 +7,23 @@ namespace InvestmentPredictor
         {
             IInvestmentCalculator calculator = new InvestmentCalculator();
             UserInterface ui = new UserInterface(calculator);
-            UserInterface.DisplayMenu("0.2"); //UI
+            UserInterface.DisplayMenu("0.3"); //UI
             decimal annualReturn = ui.GetAnnualReturn();
             Console.WriteLine($"Your annual return rate is {annualReturn}%.");
             decimal initialAmount = ui.ReadDecimal("Enter initial investment amount: ");
             int period = ui.ReadInt("How long do you want to invest? (years): ");
             decimal monthlySubsidy = ui.ReadDecimal("How much do you want to subsidize monthly?: ");
-            decimal calculatedValue = calculator.CalculatedValue(monthlySubsidy, initialAmount, annualReturn, period);
-            decimal pureReturn = calculator.getPureReturnValue(monthlySubsidy, initialAmount, annualReturn, period);
-            decimal tax = calculator.totalValueAfterTax(
-                () => calculator.CalculatedValue(monthlySubsidy, initialAmount, annualReturn, period),
-                 () => calculator.getPureReturnValue(monthlySubsidy, initialAmount, annualReturn, period),
-                  () => calculator.CalculateTaxValue(monthlySubsidy, initialAmount, annualReturn, period)
-
-                );
+            var calculatorParams=new CalculatorParams(monthlySubsidy, initialAmount, annualReturn, period);
+            decimal calculatedValue = calculator.CalculatedValue(calculatorParams);
+            decimal pureReturn = calculator.getPureReturnValue(calculatorParams);
+            decimal tax = calculator.totalValueAfterTax(calculatorParams);
             Console.WriteLine($"Total value after {period} years: {calculatedValue:C}");
 
-            //zjebane 
+            //Debug Line 
             Console.WriteLine($"Pure return value of your investment is {pureReturn:C}");
-            Console.WriteLine($"Value after tax: {tax:C}");
+
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine($"Your overall total value after tax: {tax:C}");
 
         }
 
