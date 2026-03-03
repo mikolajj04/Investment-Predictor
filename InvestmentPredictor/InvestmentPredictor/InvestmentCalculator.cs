9c328e30c0fc3aa4f57b035f95fb6364fa89725a
mikolajj04 <mikolajjussak@gmail.com>
2026-03-03 14:54:03 +0100

    style: change version to v0.3 and change tax variable name to finalNetValue

---
+++ InvestmentPredictor/InvestmentPredictor/InvestmentCalculator.cs
@@ -0,0 +1,63 @@
+﻿namespace InvestmentPredictor
+{
+    public class InvestmentCalculator : IInvestmentCalculator
+    {
+        public decimal GetIndexAnnualReturn(MarketIndex index) => index switch
+        {
+            MarketIndex.SP500 => 10.4m,
+            MarketIndex.Nasdaq100 => 14.4m,
+            MarketIndex.WIG20 => 5.2m,
+            MarketIndex.Gold => 8.5m,
+            MarketIndex.MSCIWorld => 8.9m,
+            MarketIndex.Russell2000=> 8.2m,
+            MarketIndex.MSCIEmergingMarkets => 5.9m,
+            MarketIndex.DowJones => 7.4m,
+            _ => 0m
+
+
+        };
+       public decimal CalculatedValue(CalculatorParams p)
+        {
+            decimal calculatedValue = p.initialAmount;
+            for (int year = 1; year <= p.period; year++)
+            {
+                for (int month = 1; month <= 12; month++)
+                {
+                    calculatedValue += p.monthlySubsidy;
+                    calculatedValue += calculatedValue * p.annualReturn / 100 / 12;
+                }
+
+
+            }
+            return calculatedValue;
+        }
+
+      public  decimal GetPureReturnValue(CalculatorParams p)
+        {
+            decimal calculatedValue = CalculatedValue(p);
+
+            decimal sumOfSubsidy = p.initialAmount + (p.monthlySubsidy * 12 * p.period);
+
+
+            return calculatedValue-sumOfSubsidy;
+
+
+
+        }
+
+        public decimal CalculateTaxValue(CalculatorParams p)
+        {
+            const decimal taxRate= 0.19m;
+            decimal pureProfit= GetPureReturnValue(p); ;
+            return pureProfit * taxRate;
+
+        }
+
+        public decimal TotalValueAfterTax(CalculatorParams p) {
+            return CalculatedValue(p) - CalculateTaxValue(p);
+        }
+        
+
+
+    }
+}
