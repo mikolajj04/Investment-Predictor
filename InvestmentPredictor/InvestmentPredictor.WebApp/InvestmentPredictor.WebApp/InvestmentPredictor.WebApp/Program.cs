42479e685d02ea109b3fc13d723a280f44930b98
mikolajj04 <mikolajjussak@gmail.com>
2026-03-14 19:36:46 +0100

    add Blazor WebApp solution

---
+++ InvestmentPredictor/InvestmentPredictor.WebApp/InvestmentPredictor.WebApp/InvestmentPredictor.WebApp/Program.cs
@@ -0,0 +1,33 @@
+using InvestmentPredictor.WebApp.Client.Pages;
+using InvestmentPredictor.WebApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(InvestmentPredictor.WebApp.Client._Imports).Assembly);
+
+app.Run();
