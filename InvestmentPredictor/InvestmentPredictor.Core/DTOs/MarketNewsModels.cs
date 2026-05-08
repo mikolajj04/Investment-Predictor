4ccc7868847e9c5491e5e9f613851543c05186da
mikolajj04 <Mikolajjussak@gmail.com>
2026-05-08 23:39:33 +0200

    feat: add DTOs for Alpha Vantage news integration

---
+++ InvestmentPredictor/InvestmentPredictor.Core/DTOs/MarketNewsModels.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace InvestmentPredictor.Core.DTOs
+{
+    public class AlphaVantageNewsResponse
+    {
+        [JsonPropertyName("items")]
+        public string Items { get; set; }
+
+        [JsonPropertyName("feed")]
+        public List<NewsArticles> Feed { get; set; }
+    }
+
+    public class NewsArticles
+    {
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("summary")]
+        public string Summary { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+
+        [JsonPropertyName("time_published")]
+        public string TimePublished { get; set; }
+
+        [JsonPropertyName("overall_sentiment_score")]
+        public string SentimentScore { get; set; }
+    }
+
+}
