9c5b4132ec2b8ee9eba8b48b5c5ec5a08ed46d6b
mikolajj04 <Mikolajjussak@gmail.com>
2026-05-10 15:15:00 +0200

    feat: add MarketSnippet template for data base snippets

---
+++ InvestmentPredictor/InvestmentCalculator.WebApp/Entities/MarketSnippet.cs
@@ -0,0 +1,10 @@
+﻿namespace InvestmentCalculator.WebApp.Entities
+{
+    public class MarketSnippet
+    {
+        public int Id { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public string SummaryType { get; set; } = "Hourly";
+    }
+}
