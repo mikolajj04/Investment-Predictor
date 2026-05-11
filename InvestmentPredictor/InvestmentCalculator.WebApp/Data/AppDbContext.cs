d6b34d041817f0481b39e608ec43b0083c567f20
mikolajj04 <Mikolajjussak@gmail.com>
2026-05-11 12:00:00 +0200

    feat: add AppDbContext for PostgreSQL integration

---
+++ InvestmentPredictor/InvestmentCalculator.WebApp/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using InvestmentCalculator.WebApp.Entities;
+using System.Security.Cryptography.X509Certificates;
+
+namespace InvestmentCalculator.WebApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<MarketSnippet> MarketSnippets { get; set; }
+    }
+}
+
+    
