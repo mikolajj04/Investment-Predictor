88a4d8706c83194e37a9144465087672dbaa1212
mikolajj04 <Mikolajjussak@gmail.com>
2026-05-14 11:46:00 +0200

    chore: add comming soon note to the Daily News tab

--- InvestmentPredictor/InvestmentPredictor.Core/Entities/MarketSnippet.cs
+++ InvestmentPredictor/InvestmentPredictor.Core/Entities/MarketSnippet.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string Content { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public string SummaryType { get; set; } = "Hourly";
+        public string SummaryType { get; set; } = "Daily";
     }
 }
